0bad33d1cf8bc74bc1f82fdce8c738d9ed0a579d
EcceDev <117386916+Eccedev@users.noreply.github.com>
2025-06-27 13:31:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const games = [
     { id: 6, title: "Hero LucaWeb3 Services Theme", description: "Dale vida a tu idea Web3", image: "./assets/hero-luca-web.png", url: "https://web-hero-luca.vercel.app/", adult: false, type: "project" },
     { id: 7, title: "$LUCA mEmE coin", description: "A digital microbe mEmE coin for everyone", image: "./assets/luca-coin.png", url: "https://lucacoin.vercel.app/", adult: false, type: "project" },
     { id: 8, title: "Pomodoro SkillsUp App", description: "Mejora conocimientos con un sistema eficaz", image: "./assets/pomodoroskillsup.png", url: "https://pomodoro-skillsup.vercel.app/", adult: false, type: "project" },
+    { id: 9, title: "Blog website theme ", description: "Un blog de categoría...y etiquetas", image: "./assets/blog-website.jpg", url: "https://blog-website-theme.vercel.app/", adult: false, type: "project" }
 ];
 
 // Elementos del DOM

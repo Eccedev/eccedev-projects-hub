5bf8a2e8f89a7ce89233631b6f18969889d172b4
EcceDev <117386916+Eccedev@users.noreply.github.com>
2026-02-16 11:58:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const games = [
     { id: 7, title: "$LUCA mEmE coin", description: "A digital microbe mEmE coin for everyone", image: "./assets/luca-coin.png", url: "https://lucacoin.vercel.app/", adult: false, type: "project" },
     { id: 8, title: "Pomodoro SkillsUp App", description: "Mejora conocimientos con un sistema eficaz", image: "./assets/pomodoroskillsup.png", url: "https://pomodoro-skillsup.vercel.app/", adult: false, type: "project" },
     { id: 9, title: "Blog website theme ", description: "Un blog de categoría...y etiquetas", image: "./assets/blog-website.jpg", url: "https://blog-website-theme.vercel.app/", adult: false, type: "project" },
-    { id: 10, title: "CRM NoSeMeEscapa ", description: "Un CRM para profesionales>clientes", image: "./assets/crm-nosemeescapa.jpg", url: "https://github.com/Eccedev/NoSeMeEscapa-CRM/tree/main", adult: false, type: "project" }
+    { id: 10, title: "CRM NoSeMeEscapa ", description: "Un CRM para profesionales>clientes", image: "./assets/crm-nosemeescapa.JPG", url: "https://github.com/Eccedev/NoSeMeEscapa-CRM/tree/main", adult: false, type: "project" }
 ];
 
 // Elementos del DOM
